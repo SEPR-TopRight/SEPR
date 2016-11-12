da39166601a563659f4a7842c0c0ff20549e3c45
Andrew-Bennett <andrew@localhost>
2016-11-12 01:23:17 +0000

    test project upload

---
+++ RoboticonColony/src/Main.java
@@ -0,0 +1,18 @@
+
+import org.lwjgl.*;
+
+public class Main {
+
+	/**
+	 * Start of the program. //TODO Write better comments
+	 * 
+	 * @author andrew
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
