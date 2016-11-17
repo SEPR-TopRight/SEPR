f7fa5ab42b3f1084ab168bab3b999b4185012b18
jcn509 <jcn509@york.ac.uk>
2016-11-17 17:17:32 +0000

    Add files via upload
    Sorted out Java doc comments, fixed some things, added some things

--- RoboticonColony/src/MarketInventory.java
+++ RoboticonColony/src/MarketInventory.java
@@ -1,57 +1,76 @@
 /**
- * A class used to keep track of the resources that a market object is in possession of
+ * A data structure used to keep track of the market's inventory.
+ * <p>
+ * Keeps track of:
+ * 	The number of units of ore the market is in possession of
+ * 	The number of units of energy the market is in possession of
+ * 	The number of roboticons the market is in possession of
+ * <p>
  * @author jcn509
  *
  */
 
 public class MarketInventory extends Inventory{
 	private int roboticonQuantity;
+	
 	/**
-	 * 
-	 * @param oreQuantity the initial quantity of ore to be stored in the inventory
-	 * @param energyQuantity the initial quantity of energy to be stored in the inventory
-	 * @param roboticonQuantity the intiail quantity of roboticons to be stored in the market
+	 * Constructor
+	 * @param oreQuantity The initial quantity of ore to be stored in the inventory.
+	 * @param energyQuantity The initial quantity of energy to be stored in the inventory.
+	 * @param roboticonQuantity The initial quantity of roboticons to be stored in the market.
 	 */
 	public MarketInventory(int oreQuantity, int energyQuantity, int roboticonQuantity){
-		super(oreQuantity,energyQuantity);
+		super(oreQuantity,energyQuantity); 
 		initialiseRoboticonQuantity(roboticonQuantity);
 	}
+	
 	/**
-	 * Set the initial quantity of roboticons to be stored in the market
-	 * @param roboticonQuantity the quantity of roboticons to be stored in the market
+	 * Set the initial quantity of roboticons to be stored in the market.
+	 * <p>
+	 * Ensures that the number of roboticons to be stored is not negative.
+	 * </p>
+	 * @param roboticonQuantity The quantity of roboticons to be stored in the inventory.
 	 */
 	private void initialiseRoboticonQuantity(int roboticonQuantity){
-		if(roboticonQuantity<0)
+		if(roboticonQuantity<0) // Cannot store a negative quantity of roboticons.
 			throw new IllegalArgumentException("roboticonQuantity must be >=0");
 		this.roboticonQuantity = roboticonQuantity;
 	}
-	public MarketInventory(){
-		super();
-		initialiseRoboticonQuantity(0);
-	}
+	
 	/**
-	 * 
-	 * @return the quantity of roboticons stored in the inventory
+	 * Returns the quantity of roboticons that are stored in the inventory.
+	 * @return The quantity of roboticons stored in the inventory.
 	 */
 	public int getRoboticonQuantity(){
 		return roboticonQuantity;
 	}
+	
 	/**
-	 * increases the quantity of roboticons stored in the market by the given amount
-	 * @param roboticonQuantityIncrease the number of roboticons to be added to the inventory
+	 * Increases the quantity of roboticons stored in the inventory by the given amount.
+	 * <p>
+	 * Ensures that the number of roboticons to add to the inventory is not negative.
+	 * </p>
+	 * @param roboticonQuantityIncrease The number of roboticons to be added to the inventory.
 	 */
 	public void increaseRoboticonQuantity(int roboticonQuantityIncrease){
-		if(roboticonQuantityIncrease<=0)
+		if(roboticonQuantityIncrease < 0) // Cannot add a negative quantity of roboticons.
 			throw new IllegalArgumentException("roboticonQuantityIncrease must be positive");
 		roboticonQuantity += roboticonQuantityIncrease;
 	}
+	
 	/**
-	 * decreases the quantity of roboticons stored in the market by the given amount
-	 * @param roboticonQuantityDecrease the number of roboticons to be removed from the market
+	 * Decreases the quantity of roboticons stored in the market by the given amount.
+	 * <p>
+	 * Ensures that the number of roboticons to be taken away from the inventory is not negative.
+	 * Ensures that the number of roboticons to be taken away is not greater than the number currently stored.
+	 * </p>
+	 * @param roboticonQuantityDecrease The number of roboticons to be removed from the market.
 	 */
 	public void decreaseRoboticonQuantity(int roboticonQuantityDecrease){
-		if(roboticonQuantityDecrease<=0)
+		if(roboticonQuantityDecrease < 0) // Cannot take away a negative quantity of roboticons.
 			throw new IllegalArgumentException("roboticonQuantityDecrease must be positive");
+		if(roboticonQuantity - roboticonQuantityDecrease < 0) // Cannot end up with a negative number of roboticons being stored.
+			throw new IllegalArgumentException("roboticonQuantityDecrease must not be greater than roboticonQuantity");
 		roboticonQuantity -= roboticonQuantityDecrease;
 	}
 }
--- RoboticonColony/src/RoboticonCustomisation.java
+++ RoboticonColony/src/RoboticonCustomisation.java
@@ -1,7 +1,6 @@
 /**
  * An enumerated type used to denote the customisation type of roboticons
  * @author jcn509
- *
  */
 enum RoboticonCustomisation {ORE("ore"), ENERGY("energy"), UNCUSTOMISED("uncustomised");
 	private final String name; // A string denoting the name of the category (used when throwing exceptions)
