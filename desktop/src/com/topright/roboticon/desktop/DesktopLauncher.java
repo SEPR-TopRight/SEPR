118e93a43c8b84cf8bb862ad1f13a3e77c35564b
Andrew-Bennett <andrew@nas10-240-145-209.york.ac.uk>
2016-11-22 12:05:19 +0000

    pls work

--- desktop/src/com/topright/roboticon/desktop/DesktopLauncher.java
+++ desktop/src/com/topright/roboticon/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.topright.roboticon.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1680;
+		config.height = 1050;
 		new LwjglApplication(new Main(), config);
 	}
 }
